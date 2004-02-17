0174be1e38ae1e5f90428a8da26e278f65b4ed42
nadezhin <nadezhin@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-02-17 04:31:26 +0000

    Netlist
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@613 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ srcj/com/sun/electric/database/network/Netlist.java
@@ -0,0 +1,352 @@
+/* -*- tab-width: 4 -*-
+ *
+ * Electric(tm) VLSI Design System
+ *
+ * File: Netlist.java
+ *
+ * Copyright (c) 2003 Sun Microsystems and Static Free Software
+ *
+ * Electric(tm) is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Electric(tm) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Electric(tm); see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, Mass 02111-1307, USA.
+ */
+package com.sun.electric.database.network;
+
+import com.sun.electric.database.hierarchy.Export;
+import com.sun.electric.database.hierarchy.Cell;
+// import com.sun.electric.database.hierarchy.Library;
+import com.sun.electric.database.hierarchy.Nodable;
+// import com.sun.electric.database.hierarchy.NodeUsage;
+import com.sun.electric.database.prototype.NodeProto;
+import com.sun.electric.database.prototype.PortProto;
+import com.sun.electric.database.topology.ArcInst;
+// import com.sun.electric.database.topology.NodeInst;
+import com.sun.electric.database.topology.PortInst;
+// import com.sun.electric.database.variable.ElectricObject;
+// import com.sun.electric.database.variable.TextDescriptor;
+// import com.sun.electric.database.variable.Variable;
+// import com.sun.electric.technology.PrimitiveNode;
+// import com.sun.electric.technology.PrimitivePort;
+// import com.sun.electric.technology.Technology;
+// import com.sun.electric.technology.technologies.Schematics;
+// import com.sun.electric.tool.Tool;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ConcurrentModificationException;
+import java.util.Iterator;
+// import java.util.Map;
+
+/**
+ * This is the Netlist class. It contains information about electric
+ * networks of a cell for a given set of options. Networks are 0-based
+ * indices. First positions occupies indices of networks connected to
+ * global signals, then networks connected to exports, and then local
+ * networks.
+ */
+public class Netlist
+{
+
+	// -------------------------- private data ---------------------------------
+
+	/** NetCell which owns this Netlist. */
+	NetCell netCell;
+
+	/**
+	 * The modCount value that the netlist believes that the backing
+	 * netCell should have.  If this expectation is violated, the netlist
+	 * has detected concurrent modification.
+	 */
+	int expectedModCount;
+
+	/** An equivalence map of PortInsts and NetNames. */
+	int[] netMap;
+	int[] nm_net;
+
+	/** An array of JNetworks in this Cell. */
+	private JNetwork[] networks;
+
+	// ---------------------- package methods -----------------
+
+	/**
+	 * The constructor of Netlist object..
+	 */
+	Netlist(NetCell netCell) {
+		this.netCell = netCell;
+		expectedModCount = netCell.modCount;
+	}
+
+	void initNetMap(int size) {
+		if (netMap == null || netMap.length != size) {
+			netMap = new int[size];
+			nm_net = new int[size];
+		}
+		for (int i = 0; i < netMap.length; i++) netMap[i] = i;
+	}
+
+	void initNetworks() {
+		closureMap(netMap);
+		int k = 0;
+		for (int i = 0; i < netMap.length; i++) {
+			if (netMap[i] == i) k++;
+		}
+		if (networks == null || networks.length != k) {
+			networks = new JNetwork[k];
+		}
+		Arrays.fill(networks, null);
+		
+		k = 0;
+		for (int i = 0; i < netMap.length; i++) {
+			if (netMap[i] == i) {
+				networks[k] = new JNetwork(this);
+				nm_net[i] = k;
+				k++;
+			} else {
+				nm_net[i] = nm_net[netMap[i]];
+			}
+		}
+	}
+
+	/**
+	 * Merge classes of equivalence map to which elements a1 and a2 belong.
+	 */
+	final void connectMap(int a1, int a2) {
+		//System.out.println("connectMap "+a1+" "+a2);
+		Netlist.connectMap(netMap, a1, a2);
+	}
+
+	/**
+	 * Merge classes of equivalence map to which elements a1 and a2 belong.
+	 */
+	private static void connectMap(int[] map, int a1, int a2)
+	{
+		int m1, m2, m;
+
+		for (m1 = a1; map[m1] != m1; m1 = map[m1]);
+		for (m2 = a2; map[m2] != m2; m2 = map[m2]);
+		m = m1 < m2 ? m1 : m2;
+
+		for (;;)
+		{
+			int k = map[a1];
+			map[a1] = m;
+			if (a1 == k) break;
+			a1 = k;
+		}
+		for (;;)
+		{
+			int k = map[a2];
+			map[a2] = m;
+			if (a2 == k) break;
+			a2 = k;
+		}
+	}
+
+	/**
+	 * Obtain canonical representation of equivalence map.
+	 */
+	private static void closureMap(int[] map)
+	{
+		for (int i = 0; i < map.length; i++)
+		{
+			map[i] = map[map[i]];
+		}
+	}
+
+	/**
+	 * Obtain canonical representation of equivalence map.
+	 */
+	private static boolean equalMaps(int[] map1, int[] map2)
+	{
+		if (map1.length != map2.length) return false;
+		for (int i = 0; i < map1.length; i++)
+		{
+			if (map1[i] != map2[i]) return false;
+		}
+		return true;
+	}
+
+	JNetwork getNetworkByMap(int mapOffset) { return networks[nm_net[mapOffset]]; }
+
+	private final void checkForModification() {
+		if (expectedModCount != netCell.modCount)
+			throw new ConcurrentModificationException();
+	}
+
+	// ---------------------- public methods -----------------
+
+	/**
+	 * Get an iterator over all of the Nodables of this Cell.
+	 * <p> Warning: before getNodables() is called, JNetworks must be
+	 * build by calling Cell.rebuildNetworks()
+	 */
+	public Iterator getNodables() {
+		checkForModification();
+		return netCell.getNodables();
+	}
+
+	public Netlist getNetlist(Nodable no) {
+		NodeProto np = no.getProto();
+		if (!(np instanceof Cell)) return null;
+		return Network.getUserNetlist((Cell)np);
+	}
+
+// 	public Global.Set getGlobals() {
+// 		checkForModification();
+// 		return netCell.getGlobals();
+// 	}
+
+	/**
+	 * Get an iterator over all of the JNetworks of this Netlist.
+	 */
+	public Iterator getNetworks() {
+		checkForModification();
+		return Arrays.asList(networks).iterator();
+	}
+
+	/*
+	 * Get network of port instance.
+	 */
+	public int getNetIndex(Nodable no, PortProto portProto, int busIndex) {
+		checkForModification();
+		if (no.getParent() != netCell.cell)
+			return -1;
+		if (portProto.getParent() != no.getProto())
+		{
+			System.out.println("Netlist.getNetwork: invalid argument portProto");
+			return -1;
+		}
+// 		if (busIndex < 0 || busIndex >= portProto.getProtoNameKey().busWidth())
+// 		{
+// 			System.out.println("Nodable.getNetwork: invalid arguments busIndex="+busIndex+" portProto="+portProto);
+// 			return -1;
+// 		}
+		int netMapIndex = netCell.getNetMapOffset(no, portProto, busIndex);
+		if (netMapIndex < 0) return -1;
+		return nm_net[netMapIndex];
+	}
+
+	/*
+	 * Get network of export.
+	 */
+	public int getNetIndex(Export export, int busIndex) {
+		checkForModification();
+		if (export.getParent() != netCell.cell) return -1;
+		if (busIndex < 0 || busIndex >= export.getProtoNameKey().busWidth())
+		{
+			System.out.println("Nodable.getNetwork: invalid arguments busIndex="+busIndex+" export="+export);
+			return -1;
+		}
+		int netMapIndex = netCell.getNetMapOffset(export, busIndex);
+		if (netMapIndex < 0) return -1;
+		return nm_net[netMapIndex];
+	}
+
+	/*
+	 * Get network index of arc.
+	 */
+	public int getNetIndex(ArcInst ai, int busIndex) {
+		checkForModification();
+		if (ai.getParent() != netCell.cell) return -1;
+		int netMapIndex = netCell.getNetMapOffset(ai, busIndex);
+		if (netMapIndex < 0) return -1;
+		return nm_net[netMapIndex];
+	}
+
+	/*
+	 * Get network of port instance.
+	 */
+	public JNetwork getNetwork(Nodable no, PortProto portProto, int busIndex) {
+		int netIndex = getNetIndex(no, portProto, busIndex);
+		if (netIndex < 0) return null;
+		return networks[netIndex];
+	}
+
+	/*
+	 * Get network of port instance.
+	 */
+	public JNetwork getNetwork(PortInst pi) {
+		PortProto portProto = pi.getPortProto();
+		if (portProto.getProtoNameKey().isBus())
+		{
+			System.out.println("PortInst.getNetwork() was called for instance of bus port "+portProto.getProtoName());
+			return null;
+		}
+		return getNetwork(pi.getNodeInst(), portProto, 0);
+	}
+
+	/*
+	 * Get network of export.
+	 */
+	public JNetwork getNetwork(Export export, int busIndex) {
+		int netIndex = getNetIndex(export, busIndex);
+		if (netIndex < 0) return null;
+		return networks[netIndex];
+	}
+
+	/*
+	 * Get network of arc.
+	 */
+	public JNetwork getNetwork(ArcInst ai, int busIndex) {
+		int netIndex = getNetIndex(ai, busIndex);
+		if (netIndex < 0) return null;
+		return networks[netIndex];
+	}
+
+	/**
+	 * Method to return either the network name or the bus name on this ArcInst.
+	 * @return the either the network name or the bus name on this ArcInst.
+	 */
+	public String getNetworkName(ArcInst ai) {
+		checkForModification();
+		if (ai.getParent() != netCell.cell) return null;
+		int busWidth = netCell.getBusWidth(ai);
+		if (busWidth > 1) {
+			return netCell.getBusName(ai).toString();
+		}
+		JNetwork network = getNetwork(ai, 0);
+		if (network == null) return null;
+		return network.describe();
+	}
+
+	/**
+	 * Method to return the bus width on an Export.
+	 * @param e the Export to examine.
+	 * @return the bus width of the Export.
+	 */
+	public int getBusWidth(Export e)
+	{
+		return e.getProtoNameKey().busWidth();
+	}
+
+	/**
+	 * Method to return the bus width on this ArcInst.
+	 * @return the either the bus width on this ArcInst.
+	 */
+	public int getBusWidth(ArcInst ai) {
+		checkForModification();
+		if (ai.getParent() != netCell.cell) return 0;
+		return netCell.getBusWidth(ai);
+	}
+
+	/**
+	 * Returns a printable version of this Netlist.
+	 * @return a printable version of this Netlist.
+	 */
+	public String toString()
+	{
+		return "Netlist of " + netCell.cell;
+	}
+
+}
