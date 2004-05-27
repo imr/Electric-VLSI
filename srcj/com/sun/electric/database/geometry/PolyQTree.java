18b33b2e58c85699cb8e8f326f97ad774105da61
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-05-27 01:18:00 +0000

    second version with search function
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@798 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/geometry/PolyQTree.java
+++ srcj/com/sun/electric/database/geometry/PolyQTree.java
@@ -41,32 +41,198 @@ public class PolyQTree {
 	{
 		;
 	}
-	public void addTreeRoot(Object obj, Rectangle2D bound)
+	/**
+	 * Print all nodes in the tree. Debugging purpose only!.
+	 */
+	public void print()
 	{
-		if (layers.get(obj) == null)
+		for (Iterator it = layers.values().iterator(); it.hasNext();)
 		{
-			PolyQNode root = new PolyQNode(bound);
-			layers.put(obj, root);
+			PolyQNodeRoot root = (PolyQNodeRoot)(it.next());
+			if (root != null)
+				root.print();
 		}
 	}
 
-	private class PolyQNode {
-		private Rectangle2D box;
+	/**
+	 * Given a layer, insert the object obj into the qTree associated.
+	 * @param layer Given layer to work with
+	 * @param box Bounding box of the cell containing the layer
+	 * @param obj Poly object to insert
+	 */
+	public void insert(Object layer, Rectangle2D box, Rectangle2D obj)
+	{
+		PolyQNodeRoot root = (PolyQNodeRoot)layers.get(layer);
+
+		if (root == null)
+		{
+			root = new PolyQNodeRoot(box);
+			layers.put(layer, root);
+		};
+		// check first if there is some overlap and removes previous elements
+		obj = root.removeObjects(box, obj);
+		root.insert(box, obj);
+	}
+
+	private static class PolyQNode
+    {
 		private List nodes;
 		private PolyQNode[] children;
 
 		/**
-		 * @param rect the bounding box of the tree leaf
+		 *
 		 */
-		public PolyQNode (Rectangle2D rect)
+		private PolyQNode () { ; }
+
+		private int getQuadrants(double centerX, double centerY, Rectangle2D box)
 		{
-			box = rect;
+		   	int loc = 0;
+
+			if (box.getMinY() < centerY)
+			{
+				// either 0 or 1 quadtrees
+				if (box.getMinX() < centerX)
+					loc |= 1 << 0;
+				if (box.getMaxX() > centerX)
+					loc |= 1 << 1;
+			}
+			if (box.getMaxY() > centerY)
+			{
+				// the other quadtrees
+				if (box.getMinX() < centerX)
+					loc |= 1 << 2;
+				if (box.getMaxX() > centerX)
+					loc |= 1 << 3;
+			}
+			return loc;
 		}
 
-		public void insert(Rectangle2D obj)
+		private Rectangle2D getBox(Rectangle2D box, double centerX, double centerY, int loc)
 		{
-			if (!box.intersects(obj)) return;
+			double w = box.getWidth()/2;
+			double h = box.getHeight()/2;
+			// Values for quadtree 0
+			double x = box.getX();
+			double y = box.getY();
 
+			if ((loc >> 1 & 1) == 0)
+			{
+				x = centerX;
+			}
+			else if ((loc >> 2 & 1) == 0)
+			{
+				y = centerY;
+			}
+			else if ((loc >> 3 & 1) == 0)
+			{
+				x = centerX;
+				y = centerY;
+			}
+			return (new Rectangle2D.Double(x, y, w, h));
+		}
+
+		/**
+		 *
+		 */
+		protected void print()
+		{
+			for (Iterator it = nodes.iterator(); it.hasNext();)
+				System.out.println("Rectangle " + (Rectangle2D)it.next());
+			if (children == null) return;
+			for (int i = 0; i < PolyQTree.MAX_NUM_CHILDREN; i++)
+				children[i].print();
+		}
+
+		/**
+		 * To compact nodes if child elements have been removed
+		 * @return true if node can be removed
+		 */
+		private boolean compact()
+		{
+			System.out.println("To implement") ;
+			return (false);
+		}
+
+		/**
+		 * Removes from tree all objects overlapping with obj. Returns the overlapping region.
+		 * @param box
+		 * @param obj
+		 * @return
+		 */
+		protected Rectangle2D removeObjects(Rectangle2D box, Rectangle2D obj)
+		{
+			double centerX = box.getCenterX();
+            double centerY = box.getCenterY();
+
+            // Node has been split
+			if (children != null)
+			{
+				int loc = getQuadrants(centerX, centerY, obj);
+				for (int i = 0; i < PolyQTree.MAX_NUM_CHILDREN; i++)
+				{
+					if (((loc >> i) & 1) == 0)
+					{
+						Rectangle2D bb = getBox(box, centerX, centerY, i);
+
+						obj = children[i].removeObjects(bb, obj);
+						if (children[i].compact())
+							children[i] = null;
+					}
+				}
+			}
+			else if (nodes == null)
+			{
+				System.out.println("Should it happen!!?");
+			}
+			else
+			{
+				List deleteList = new ArrayList();
+
+				for (Iterator it = nodes.iterator(); it.hasNext();)
+				{
+					Rectangle2D node = (Rectangle2D)it.next();
+
+					if (node.intersects(box))
+					{
+						box.add(node);
+						deleteList.add(node);
+					}
+				}
+				nodes.removeAll(deleteList);
+				if (nodes.size() == 0)
+				{
+					// not sure yet
+					nodes.clear();
+					nodes = null;
+				}
+			}
+			return (obj);
+		}
+
+		/**
+		 *
+		 * @param box
+		 * @param obj
+		 */
+		protected void insert(Rectangle2D box, Rectangle2D obj)
+		{
+			double centerX = box.getCenterX();
+            double centerY = box.getCenterY();
+
+			// Node has been split
+			if (children != null)
+			{
+				int loc = getQuadrants(centerX, centerY, obj);
+				for (int i = 0; i < PolyQTree.MAX_NUM_CHILDREN; i++)
+				{
+					if (((loc >> i) & 1) == 0)
+					{
+						Rectangle2D bb = getBox(box, centerX, centerY, i);
+
+						children[i].insert(bb, obj);
+					}
+				}
+			}
 			if (nodes == null)
 			{
 				nodes = new ArrayList();
@@ -79,26 +245,32 @@ public class PolyQTree {
 			{
 				// subdivides into 4
 				children = new PolyQNode[4];
-				Rectangle2D r = new Rectangle2D.Double();
-				double w = box.getWidth()/2;
-				double h = box.getHeight()/2;
-				double x = box.getX();
-				double y = box.getY();
 
 				for (int i = 0; i < 4; i++)
 				{
-					r.setRect(x+w*(i%2), y+h*((i+1)%2), w, h);
-					children[i] = new PolyQNode(r);
+					children[i] = new PolyQNode();
 				}
+
 				for (Iterator it = nodes.iterator(); it.hasNext();)
 				{
 					for (int i = 0; i < 4; i++)
 					{
-						children[i].insert((Rectangle2D)it.next());
+						Rectangle2D bb = getBox(box, centerX, centerY, i);
+						children[i].insert(bb, (Rectangle2D)it.next());
 					}
 				}
-				//remove list?
+				nodes.clear(); // not sure about this clear yet
+				nodes = null;
 			}
+		}
+	}
+	private class PolyQNodeRoot extends PolyQTree.PolyQNode
+	{
+		private Rectangle2D box; // contain the bounding box of the entire cell
+
+		public PolyQNodeRoot (Rectangle2D bound)
+		{
+			box = bound;
 		}
 	}
 }
