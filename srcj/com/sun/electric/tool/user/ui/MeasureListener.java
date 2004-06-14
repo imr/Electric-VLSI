556833282876d4920d702346abe84626f742b22c
gainsley <gainsley@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-06-14 19:46:19 +0000

    can draw multiple measures, measures now work when zoom/panning
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@903 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/user/ui/MeasureListener.java
+++ srcj/com/sun/electric/tool/user/ui/MeasureListener.java
@@ -27,7 +27,7 @@ import com.sun.electric.database.hierarchy.Cell;
 import com.sun.electric.database.text.TextUtils;
 import com.sun.electric.tool.user.Highlight;
 
-import java.awt.Dimension;
+import java.awt.*;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseMotionListener;
@@ -43,7 +43,10 @@ public class MeasureListener
 {
 	public static MeasureListener theOne = new MeasureListener();
 	private static double lastMeasuredDistanceX = 0, lastMeasuredDistanceY = 0;
-	private int startX, startY;
+    private static boolean measuring = false;               // true if drawing measure line
+    private static Highlight lastMeasure = null;
+    private static Highlight lastMeasureLine = null;
+	private Point2D dbStart;                 // start of measure in database units
 
 	private MeasureListener() {}
 
@@ -54,45 +57,140 @@ public class MeasureListener
 		return dim;
 	}
 
-	public void mousePressed(MouseEvent evt)
-	{
-		startX = evt.getX();
-		startY = evt.getY();
-	}
+    private void startMeasure(Point2D dbStart) {
+        this.dbStart = dbStart;
+        measuring = true;
+        lastMeasure = null;
+        lastMeasureLine = null;
+    }
 
+    private void dragOutMeasure(EditWindow wnd, Point2D dbPoint) {
+
+        if (measuring) {
+            //Highlight.clear();
+            Point2D start = dbStart;
+            Point2D end = dbPoint;
+            if (lastMeasureLine != null) Highlight.remove(lastMeasureLine);
+            lastMeasureLine = Highlight.addLine(start, end, wnd.getCell());
+
+            lastMeasuredDistanceX = Math.abs(start.getX() - end.getX());
+            lastMeasuredDistanceY = Math.abs(start.getY() - end.getY());
+            Point2D center = new Point2D.Double((start.getX() + end.getX()) / 2, (start.getY() + end.getY()) / 2);
+            double dist = start.distance(end);
+            if (lastMeasure != null) Highlight.remove(lastMeasure);
+            lastMeasure = Highlight.addMessage(wnd.getCell(), TextUtils.formatDouble(dist), center);
+
+            Highlight.finished();
+            wnd.clearDoingAreaDrag();
+            wnd.repaint();
+        }
+    }
+
+    private void finishMeasure(EditWindow wnd) {
+        if (measuring)
+            measuring = false;
+        else {
+            // clear measures from the screen if user cancels twice in a row
+            Highlight.clear();
+            Highlight.finished();
+            wnd.repaint();
+        }
+    }
+
+
+    // ------------------------ Mouse Listener Stuff -------------------------
+
+    public void mousePressed(MouseEvent evt)
+    {
+        if (evt.getSource() instanceof EditWindow)
+        {
+            int newX = evt.getX();
+            int newY = evt.getY();
+            EditWindow wnd = (EditWindow)evt.getSource();
+            Point2D dbMouse = wnd.screenToDatabase(newX, newY);
+            EditWindow.gridAlign(dbMouse);
+            if (isLeftMouse(evt)) {
+                startMeasure(dbMouse);
+            }
+            if (isRightMouse(evt)) {
+                finishMeasure(wnd);
+            }
+        }
+    }
+
+    public void mouseDragged(MouseEvent evt)
+    {
+        if (evt.getSource() instanceof EditWindow)
+        {
+            int newX = evt.getX();
+            int newY = evt.getY();
+            EditWindow wnd = (EditWindow)evt.getSource();
+            Point2D dbMouse = wnd.screenToDatabase(newX, newY);
+            EditWindow.gridAlign(dbMouse);
+            dragOutMeasure(wnd, dbMouse);
+        }
+    }
+
+    public void mouseMoved(MouseEvent evt) { mouseDragged(evt); }
 	public void mouseClicked(MouseEvent evt) {}
 	public void mouseEntered(MouseEvent evt) {}
 	public void mouseExited(MouseEvent evt) {}
-	public void mouseMoved(MouseEvent evt) {}
 	public void mouseReleased(MouseEvent evt) {}
 	public void mouseWheelMoved(MouseWheelEvent evt) {}
 
-	public void mouseDragged(MouseEvent evt)
-	{
-		int newX = evt.getX();
-		int newY = evt.getY();
- 		if (evt.getSource() instanceof EditWindow)
-		{
-			EditWindow wnd = (EditWindow)evt.getSource();
 
-			Highlight.clear();
-			Point2D start = wnd.screenToDatabase(startX, startY);
-			EditWindow.gridAlign(start);
-			Point2D end = wnd.screenToDatabase(newX, newY);
-			EditWindow.gridAlign(end);
-			Highlight.addLine(start, end, wnd.getCell());
-			lastMeasuredDistanceX = Math.abs(start.getX() - end.getX());
-			lastMeasuredDistanceY = Math.abs(start.getY() - end.getY());
-			Point2D center = new Point2D.Double((start.getX() + end.getX()) / 2, (start.getY() + end.getY()) / 2);
-			double dist = start.distance(end);
-			Highlight.addMessage(wnd.getCell(), TextUtils.formatDouble(dist), center);
-			Highlight.finished();
-			wnd.clearDoingAreaDrag();
-			wnd.repaint();
-		}
-	}
+	public void keyPressed(KeyEvent evt) {
+        int chr = evt.getKeyCode();
 
-	public void keyPressed(KeyEvent evt) {}
+        if (evt.getSource() instanceof EditWindow)
+        {
+            EditWindow wnd = (EditWindow)evt.getSource();
+
+            if (chr == KeyEvent.VK_ESCAPE) {
+                finishMeasure(wnd);
+            }
+        }
+    }
 	public void keyReleased(KeyEvent evt) {}
 	public void keyTyped(KeyEvent evt) {}
+
+
+    // mac stuff
+    private static final boolean isMac = System.getProperty("os.name").toLowerCase().startsWith("mac");
+
+    /**
+     * See if event is a left mouse click.  Platform independent.
+     */
+    private boolean isLeftMouse(MouseEvent evt) {
+        if (isMac) {
+            if (!evt.isMetaDown()) {
+                if ((evt.getModifiers() & MouseEvent.BUTTON1_MASK) == MouseEvent.BUTTON1_MASK)
+                    return true;
+            }
+        } else {
+            if ((evt.getModifiers() & MouseEvent.BUTTON1_MASK) == MouseEvent.BUTTON1_MASK)
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * See if event is a right mouse click.  Platform independent.
+     * One-button macs: Command + click == right mouse click.
+     */
+    public static boolean isRightMouse(MouseEvent evt) {
+        if (isMac) {
+            if (evt.isMetaDown()) {
+                if ((evt.getModifiers() & MouseEvent.BUTTON1_MASK) == MouseEvent.BUTTON1_MASK)
+                    return true;
+            }
+            if ((evt.getModifiers() & MouseEvent.BUTTON3_MASK) == MouseEvent.BUTTON3_MASK)
+                return true;
+        } else {
+            if ((evt.getModifiers() & MouseEvent.BUTTON3_MASK) == MouseEvent.BUTTON3_MASK)
+                return true;
+        }
+        return false;
+    }
+
 }
