708a53e43db84a4dce3ce81b0951efa4421b0285
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-07-02 19:34:30 +0000

    Moving gif files to resources folders
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@1020 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ srcj/com/sun/electric/tool/user/Resources.java
@@ -0,0 +1,43 @@
+/* -*- tab-width: 4 -*-
+ *
+ * Electric(tm) VLSI Design System
+ *
+ * File: WaveformWindow.java
+ *
+ * Copyright (c) 2004 Sun Microsystems and Static Free Software
+ *
+ * Electric(tm) is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Electric(tm) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Electric(tm); see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, Mass 02111-1307, USA.
+ */
+package com.sun.electric.tool.user;
+
+import javax.swing.ImageIcon;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gilda
+ * Date: Jul 2, 2004
+ * Time: 11:25:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+/** public class to handle resources like icons/images */
+public class Resources {
+	private static final String resourceLocation = "resources/";
+
+	public static ImageIcon getResource(Class theClass, String iconName)
+	{
+		return (new ImageIcon(theClass.getResource(resourceLocation+iconName)));
+	}
+}
