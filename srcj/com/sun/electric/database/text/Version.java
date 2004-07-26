90377bcef75a931d7eea3c530bb1d6dd39ac6fe9
gainsley <gainsley@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-07-26 23:31:53 +0000

    bug fixes
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@1160 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/text/Version.java
+++ srcj/com/sun/electric/database/text/Version.java
@@ -45,7 +45,7 @@ public class Version
 	/**
 	 * This is the current version of Electric
 	 */
-	private static final String CURRENT = "8.01a";
+	private static final String CURRENT = "8.01b";
 
 	private int major;
 	private int minor;
