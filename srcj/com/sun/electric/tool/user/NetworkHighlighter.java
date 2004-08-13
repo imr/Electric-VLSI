0eb09e80213b0f96932c7b6299c6b9a621791ae0
gainsley <gainsley@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-08-13 01:03:14 +0000

    hierarchical highlighting of networks
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@1258 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ srcj/com/sun/electric/tool/user/NetworkHighlighter.java
@@ -0,0 +1,232 @@
+package com.sun.electric.tool.user;
+
+import com.sun.electric.database.hierarchy.HierarchyEnumerator;
+import com.sun.electric.database.hierarchy.Nodable;
+import com.sun.electric.database.hierarchy.Cell;
+import com.sun.electric.database.hierarchy.Export;
+import com.sun.electric.database.network.JNetwork;
+import com.sun.electric.database.network.Netlist;
+import com.sun.electric.database.variable.VarContext;
+import com.sun.electric.database.topology.ArcInst;
+import com.sun.electric.database.topology.PortInst;
+import com.sun.electric.database.topology.NodeInst;
+import com.sun.electric.database.geometry.Poly;
+import com.sun.electric.technology.PrimitiveNode;
+
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.awt.geom.AffineTransform;
+
+/**
+ * This class is used for hierarchical highlighting of networks
+ */
+public class NetworkHighlighter extends HierarchyEnumerator.Visitor {
+
+    private Cell cell;
+    private Netlist netlist;
+    private JNetwork net;
+    private int netID;
+    private int startDepth;
+    private int endDepth;
+    private int currentDepth;
+    private Highlighter highlighter;
+
+    private NetworkHighlighter(Cell cell, Netlist netlist, JNetwork net, int startDepth, int endDepth) {
+        this.cell = cell;
+        this.netlist = netlist;
+        this.net = net;
+        this.startDepth = startDepth;
+        this.endDepth = endDepth;
+        currentDepth = 0;
+        highlighter = new Highlighter();
+    }
+
+    /**
+     * Returns a list of Highlight objects that draw lines and boxes over
+     * instances that denote the location of objects in that instance that
+     * are connected to net. The depth of the search is specified by startDepth
+     * and endDepth, which start at 0. If both are set to zero, only objects in the
+     * cell Cell are highlighted.
+     * @param cell the cell in which to highlight objects
+     * @param netlist the netlist for the cell
+     * @param net objects connected to this network will be highlighted
+     * @param startDepth to start depth of the hierarchical search
+     * @return endDepth the end depth of the hierarchical search
+     */
+    public static List getHighlights(Cell cell, Netlist netlist, JNetwork net, int startDepth, int endDepth) {
+        NetworkHighlighter networkHighlighter = new NetworkHighlighter(cell, netlist, net, startDepth, endDepth);
+
+        HierarchyEnumerator.enumerateCell(cell, VarContext.globalContext, netlist, networkHighlighter);
+
+        return networkHighlighter.highlighter.getHighlights();
+    }
+
+
+
+    public boolean enterCell(HierarchyEnumerator.CellInfo info) {
+        if (currentDepth < startDepth) {
+            currentDepth++;
+            return true;
+        }
+
+        if (currentDepth == 0) {
+            // set the global net ID that will be used in sub cells
+            netID = info.getNetID(net);
+
+            addNetworkObjects();
+        } else {
+            addNetworkPolys(info);
+        }
+
+        currentDepth++;
+        if (currentDepth > endDepth) return false;
+        return true;
+    }
+
+    public void exitCell(HierarchyEnumerator.CellInfo info) {
+        currentDepth--;
+    }
+
+    public boolean visitNodeInst(Nodable ni, HierarchyEnumerator.CellInfo info) {
+        return true;
+    }
+
+
+    /**
+     * Get objects connected to network 'net' in Cell 'cell' with Netlist 'netlist.
+     * This will grab all arcs on net if 'arcs' is not null, it will grab all
+     * ports if both 'arcs' and 'ports' are not null, and will grab all
+     * exports if 'exports' is not null.
+     * @param cell
+     * @param netlist
+     * @param net
+     * @param arcs list of ArcInsts on network
+     * @param ports list of PortsInsts on network
+     * @param exports list of Exports on network
+     */
+    private static void getNetworkObjects(Cell cell, Netlist netlist, JNetwork net,
+                                       List arcs, List ports, List exports) {
+        if (arcs != null) {
+            List nodesAdded = new ArrayList();
+
+            // all arcs on the network
+            for(Iterator aIt = cell.getArcs(); aIt.hasNext(); )
+            {
+                ArcInst ai = (ArcInst)aIt.next();
+                int width = netlist.getBusWidth(ai);
+                for(int i=0; i<width; i++)
+                {
+                    JNetwork oNet = netlist.getNetwork(ai, i);
+                    if (oNet == net)
+                    {
+                        arcs.add(ai);
+                        if (ports != null) {
+                            // also highlight end nodes of arc, if they are primitive nodes
+                            PortInst pi = ai.getHead().getPortInst();
+                            if (pi.getNodeInst().getProto() instanceof PrimitiveNode) {
+                                if (!nodesAdded.contains(pi)) {
+                                    // prevent duplicates
+                                    ports.add(pi);
+                                    nodesAdded.add(pi);
+                                }
+                            }
+                            pi = ai.getTail().getPortInst();
+                            if (pi.getNodeInst().getProto() instanceof PrimitiveNode) {
+                                if (!nodesAdded.contains(pi)) {
+                                    // prevent duplicates
+                                    ports.add(pi);
+                                    nodesAdded.add(pi);
+                                }
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        if (exports != null) {
+            // show all exports on the network
+            for(Iterator pIt = cell.getPorts(); pIt.hasNext(); )
+            {
+                Export pp = (Export)pIt.next();
+                int width = netlist.getBusWidth(pp);
+                for(int i=0; i<width; i++)
+                {
+                    JNetwork oNet = netlist.getNetwork(pp, i);
+                    if (oNet == net)
+                    {
+                        exports.add(pp);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * Add all network objects (arcs, primitive nodes) to highlighter for current cell
+     */
+    private void addNetworkObjects() {
+        // highlight objects in this cell
+        List arcs = new ArrayList();
+        List ports = new ArrayList();
+        List exports = new ArrayList();
+
+        getNetworkObjects(cell, netlist, net, arcs, ports, exports);
+
+        for (Iterator it = arcs.iterator(); it.hasNext(); ) {
+            ArcInst ai = (ArcInst)it.next();
+            highlighter.addElectricObject(ai, cell, false);
+        }
+        for (Iterator it = ports.iterator(); it.hasNext(); ) {
+            PortInst pi = (PortInst)it.next();
+            highlighter.addElectricObject(pi, cell, false);
+        }
+        for (Iterator it = exports.iterator(); it.hasNext(); ) {
+            Export ep = (Export)it.next();
+            highlighter.addElectricObject(ep, cell, false);
+        }
+    }
+
+    /**
+     * Add all network objects (arcs, primitive nodes) as poly
+     * objects, transformed by trans.
+     */
+    private void addNetworkPolys(HierarchyEnumerator.CellInfo info) {
+        Netlist netlist = info.getNetlist();
+
+        // find network in this cell that corresponds to global id
+        JNetwork localNet = null;
+        for (Iterator it = netlist.getNetworks(); it.hasNext(); ) {
+            JNetwork aNet = (JNetwork)it.next();
+            if (info.getNetID(aNet) == netID) {
+                localNet = aNet;
+                break;
+            }
+        }
+
+        // no local net that matches with global network
+        if (localNet == null) return;
+
+        Cell currentCell = info.getCell();
+        List arcs = new ArrayList();
+        List ports = new ArrayList();
+        getNetworkObjects(currentCell, netlist, localNet, arcs, ports, null);
+
+        // get polys for each object and transform them to root
+        AffineTransform trans = info.getTransformToRoot();
+        for (Iterator it = arcs.iterator(); it.hasNext(); ) {
+            ArcInst ai = (ArcInst)it.next();
+            //Poly poly = ai.get
+        }
+        for (Iterator it = ports.iterator(); it.hasNext(); ) {
+            PortInst pi = (PortInst)it.next();
+            NodeInst ni = pi.getNodeInst();
+            //Poly poly = ni.get
+        }
+
+    }
+}
