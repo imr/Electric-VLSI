075626403e466f3a2dc78c93fd8e62706d869fbf
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-10-20 18:02:55 +0000

    Fixing crash in no debug mode
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@1670 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/Job.java
+++ srcj/com/sun/electric/tool/Job.java
@@ -648,6 +648,7 @@ public abstract class Job implements ActionListener, Runnable {
      * @see #invokeExamineLater(Runnable, Object)
      */
     public static synchronized void releaseExamineLock() {
+	    if (!Main.getDebug()) return;      // only enable if DEBUG mode
         Job dummy = databaseChangesThread.getJob(Thread.currentThread());
         assert(dummy != null);
         assert(dummy instanceof InthreadExamineJob);
