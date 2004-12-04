c01c9ab7a4775fd9e1ec0bd9082f19656b1680ba
gainsley <gainsley@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2004-12-04 00:47:35 +0000

    add comparator to compare objects by toString()
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@2005 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/text/TextUtils.java
+++ srcj/com/sun/electric/database/text/TextUtils.java
@@ -1125,6 +1125,16 @@ public class TextUtils
 		}
 	}
 
+    public static class ObjectsByToString implements Comparator
+    {
+        public int compare(Object o1, Object o2)
+        {
+            String s1 = o1.toString();
+            String s2 = o2.toString();
+            return s1.compareToIgnoreCase(s2);
+        }
+    }
+
 	public static class CellsByView implements Comparator
 	{
 		public int compare(Object o1, Object o2)
