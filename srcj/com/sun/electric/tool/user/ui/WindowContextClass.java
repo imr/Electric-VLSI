e3841396290f92c040ce13d3ad71f5eb73fb6e05
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2005-09-23 14:30:47 +0000

    attempt to get rid of WindowContext interface
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@3286 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ srcj/com/sun/electric/tool/user/ui/WindowContextClass.java
@@ -0,0 +1,75 @@
+package com.sun.electric.tool.user.ui;
+
+import com.sun.electric.tool.user.HighlightListener;
+import com.sun.electric.tool.user.Highlighter;
+import com.sun.electric.database.hierarchy.Cell;
+import com.sun.electric.database.hierarchy.Library;
+import com.sun.electric.database.variable.VarContext;
+
+import javax.swing.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Gilda
+ * Date: Sep 22, 2005
+ * Time: 10:25:08 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+/**
+ * This class defines the right-side of a windowframe (the contents, as opposed to the explorer tree).
+ */
+public abstract class WindowContextClass extends JPanel
+        implements HighlightListener
+{
+	/** the cell that is in the window */					protected Cell cell;
+	/** Highlighter for this window */                      protected Highlighter highlighter;
+	/** the window frame containing this editwindow */      protected WindowFrame wf;
+
+    public WindowContextClass(Cell c, WindowFrame wf)
+    {
+        this.cell = c;
+        this.wf = wf;
+
+		highlighter = new Highlighter(Highlighter.SELECT_HIGHLIGHTER, wf);
+        highlighter.addHighlightListener(this);
+    }
+
+
+    /**
+     * Method to return the cell that is shown in this window.
+     * @return the cell that is shown in this window.
+     */
+    public Cell getCell() { return cell; }
+
+    public void setCell(Cell cell)
+    {
+        this.cell = cell;
+    }
+
+    /**
+	 * Centralized version of naming windows. Might move it to class
+	 * that would replace WindowContext
+	 * @param prefix a prefix for the title.
+	 */
+	public String composeTitle(String prefix, int pageNo)
+	{
+		// StringBuffer should be more efficient
+		StringBuffer title = new StringBuffer();
+
+		if (cell != null)
+		{
+			title.append(prefix + cell.libDescribe());
+
+			if (cell.isMultiPage())
+			{
+				title.append(" - Page " + (pageNo+1));
+			}
+			if (cell.getLibrary() != Library.getCurrent())
+				title.append(" - Current library: " + Library.getCurrent().getName());
+		}
+		else
+			title.append("***NONE***");
+		return (title.toString());
+	}
+}
