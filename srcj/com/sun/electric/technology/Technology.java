11053ce45d489c3c9ef375f9efa990d9ed4317f6
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2006-02-10 19:08:46 +0000

    cleaning import
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@4019 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/technology/Technology.java
+++ srcj/com/sun/electric/technology/Technology.java
@@ -80,7 +80,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.prefs.Preferences;
 
 /**
  * Technology is the base class for all of the specific technologies in Electric.
