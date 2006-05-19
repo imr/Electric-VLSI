97cd1073363a6cd1dae0903601bf4cc03f208889
gainsley <gainsley@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2006-05-19 01:36:48 +0000

    commit last modified file
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@4655 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/cvspm/Commit.java
+++ srcj/com/sun/electric/tool/cvspm/Commit.java
@@ -143,9 +143,9 @@ public class Commit {
             String useDir = CVS.getUseDir(libsToCommit, cellsToCommit);
             StringBuffer libs = CVS.getLibraryFiles(libsToCommit, useDir);
             StringBuffer cells = CVS.getCellFiles(cellsToCommit, useDir);
-            //StringBuffer lastModifiedFiles = CVS.getDELIBLastModifiedFiles(libsToCommit, cellsToCommit, useDir);
+            StringBuffer lastModifiedFiles = CVS.getDELIBLastModifiedFiles(libsToCommit, cellsToCommit, useDir);
             StringBuffer headerFiles = CVS.getHeaderFiles(libsToCommit, cellsToCommit, useDir);
-            String commitFiles = libs + " " + headerFiles + " " + cells;
+            String commitFiles = libs + " " + headerFiles + " " + lastModifiedFiles + " " + cells;
             if (commitFiles.trim().equals("")) {
                 System.out.println("Nothing to commit");
                 exitVal = 0;
