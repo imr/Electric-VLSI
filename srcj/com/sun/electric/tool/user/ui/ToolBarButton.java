07f68a027de81441b33ed369a3b1fecf3496a0be
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2006-05-25 18:45:26 +0000

    cleaning
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@4721 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/user/ui/ToolBarButton.java
+++ srcj/com/sun/electric/tool/user/ui/ToolBarButton.java
@@ -33,7 +33,6 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.HashMap;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.accessibility.Accessible;
