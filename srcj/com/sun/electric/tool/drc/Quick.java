6c762ecf857bea7df883ab78cc84a83be607b2f9
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2006-06-09 18:24:33 +0000

    Fixing bogus min area drc errors reported on dp_stageN{lay} (Bug #1111).
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@4854 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/drc/Quick.java
+++ srcj/com/sun/electric/tool/drc/Quick.java
@@ -4661,7 +4661,8 @@ public class Quick
 		 */
 		public void exitCell(HierarchyEnumerator.CellInfo info)
         {
-            if (preProcess || doneAll) return;
+            // If abort, it should not process minArea.
+            if (preProcess || doneAll || job.checkAbort()) return;
             boolean isTopCell = info.getCell() == topCell;
 
 //            if (!isTopCell) return;     Case 5 too slow!. Case 4 is good for now
