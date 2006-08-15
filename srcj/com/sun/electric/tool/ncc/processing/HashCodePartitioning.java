da7bfc60bad63c1745827b436bf0f4565f687f7f
rkao <rkao@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2006-08-15 16:22:18 +0000

    Comment out obsolete code, old hash code partitioning
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@5070 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/ncc/processing/HashCodePartitioning.java
+++ srcj/com/sun/electric/tool/ncc/processing/HashCodePartitioning.java
@@ -21,194 +21,194 @@
  * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, Mass 02111-1307, USA.
 */
-package com.sun.electric.tool.ncc.processing;
-
-import java.util.Date;
-import java.util.Iterator;
-
-import com.sun.electric.tool.ncc.NccGlobals;
-import com.sun.electric.tool.ncc.basic.NccUtils;
-import com.sun.electric.tool.ncc.lists.LeafList;
-import com.sun.electric.tool.ncc.netlist.NetObject;
-import com.sun.electric.tool.ncc.strategy.StratAdjacent;
-import com.sun.electric.tool.ncc.strategy.StratCount;
-import com.sun.electric.tool.ncc.strategy.StratHashParts;
-import com.sun.electric.tool.ncc.strategy.StratHashWires;
-import com.sun.electric.tool.ncc.strategy.StratPortName;
-import com.sun.electric.tool.ncc.strategy.StratRandomMatch;
-import com.sun.electric.tool.ncc.strategy.StratSizes;
-import com.sun.electric.tool.ncc.trees.EquivRecord;
-
-public class HashCodePartitioning {
-	NccGlobals globals;
-	
-	private LeafList hashFrontierParts(){
-		if (globals.getParts()==null)  return new LeafList();
-		globals.status2("----- hash all Parts on frontier");
-//		LeafList frontier = StratFrontier.doYourJob(globals.getParts(), globals);
-		Iterator<EquivRecord> frontier = globals.getPartLeafEquivRecs().getNotMatched();
-		LeafList offspring = StratHashParts.doYourJob(frontier, globals);
-		return offspring;
-	}
-	
-	private LeafList hashFrontierWires(){
-		if (globals.getParts()==null)  return new LeafList();
-		globals.status2("----- hash all Wires on frontier");
-//		LeafList frontier = StratFrontier.doYourJob(globals.getWires(), globals);
-		Iterator<EquivRecord> frontier = globals.getWireLeafEquivRecs().getNotMatched();
-		LeafList offspring = StratHashWires.doYourJob(frontier, globals);
-		return offspring;
-	}
-	
-	private boolean isPartsList(LeafList el) {
-		EquivRecord er = (EquivRecord) el.get(0);
-		return er.getNetObjType()==NetObject.Type.PART;
-	}
-
-	/** Takes a list of newly divided Part/Wire EquivRecords. If any
-	 * of them match then find the match's neighbors and perform a
-	 * hash step on them to get newly divided Wire/Part
-	 * EquivRecords. Repeat until the hash step yields no newly
-	 * divided EquivRecords.
-	 * @param newDivided newly divided EquivRecords */
-	private void chaseMatched(LeafList newDivided) {
-		// if nothing divided then suppress chaseMatched messages
-		if (newDivided.size()==0) return;
-		
-		globals.status2("------ starting chaseMatched");
-		int i=0;
-		while (newDivided.size()!=0) {
-			globals.status2("------ chaseMatched pass: " + i++);
-			LeafList newMatches = newDivided.selectMatched(globals);
-			if (newMatches.size()==0) break;
-			LeafList adjacent = StratAdjacent.doYourJob(newMatches, globals);
-			if (adjacent.size()==0)  break;
-			boolean doParts = isPartsList(adjacent);
-			newDivided = 
-				doParts ? StratHashParts.doYourJob(adjacent.iterator(), globals) :
-					      StratHashWires.doYourJob(adjacent.iterator(), globals);
-		}
-		globals.status2("------ done  chaseMatched after "+i+" passes");
-		//		StratCheck.doYourJob(globals.getRoot, globals);
-		globals.status2("");
-	}
-	
-//	/**
-//	 * Takes a list of newly divided Part/Wire EquivRecords. Finds
-//	 * their neighbors and performs a hash step on them to get newly
-//	 * divided Wire/Part EquivRecords. Repeat until the hash step
-//	 * yields no newly divided EquivRecords.
-//	 * @param newDivided newly divided EquivRecords
-//	 */
-//	private void chaseDivided(LeafList newDivided) {
-//		// if nothing divided then suppress chaseDivided messages
-//		if (newDivided.size()==0) return;
+//package com.sun.electric.tool.ncc.processing;
 //
-//		globals.println("------ starting chaseDivided");
+//import java.util.Date;
+//import java.util.Iterator;
+//
+//import com.sun.electric.tool.ncc.NccGlobals;
+//import com.sun.electric.tool.ncc.basic.NccUtils;
+//import com.sun.electric.tool.ncc.lists.LeafList;
+//import com.sun.electric.tool.ncc.netlist.NetObject;
+//import com.sun.electric.tool.ncc.strategy.StratAdjacent;
+//import com.sun.electric.tool.ncc.strategy.StratCount;
+//import com.sun.electric.tool.ncc.strategy.StratHashParts;
+//import com.sun.electric.tool.ncc.strategy.StratHashWires;
+//import com.sun.electric.tool.ncc.strategy.StratPortName;
+//import com.sun.electric.tool.ncc.strategy.StratRandomMatch;
+//import com.sun.electric.tool.ncc.strategy.StratSizes;
+//import com.sun.electric.tool.ncc.trees.EquivRecord;
+//
+//public class HashCodePartitioning {
+//	NccGlobals globals;
+//	
+//	private LeafList hashFrontierParts(){
+//		if (globals.getParts()==null)  return new LeafList();
+//		globals.status2("----- hash all Parts on frontier");
+////		LeafList frontier = StratFrontier.doYourJob(globals.getParts(), globals);
+//		Iterator<EquivRecord> frontier = globals.getPartLeafEquivRecs().getNotMatched();
+//		LeafList offspring = StratHashParts.doYourJob(frontier, globals);
+//		return offspring;
+//	}
+//	
+//	private LeafList hashFrontierWires(){
+//		if (globals.getParts()==null)  return new LeafList();
+//		globals.status2("----- hash all Wires on frontier");
+////		LeafList frontier = StratFrontier.doYourJob(globals.getWires(), globals);
+//		Iterator<EquivRecord> frontier = globals.getWireLeafEquivRecs().getNotMatched();
+//		LeafList offspring = StratHashWires.doYourJob(frontier, globals);
+//		return offspring;
+//	}
+//	
+//	private boolean isPartsList(LeafList el) {
+//		EquivRecord er = (EquivRecord) el.get(0);
+//		return er.getNetObjType()==NetObject.Type.PART;
+//	}
+//
+//	/** Takes a list of newly divided Part/Wire EquivRecords. If any
+//	 * of them match then find the match's neighbors and perform a
+//	 * hash step on them to get newly divided Wire/Part
+//	 * EquivRecords. Repeat until the hash step yields no newly
+//	 * divided EquivRecords.
+//	 * @param newDivided newly divided EquivRecords */
+//	private void chaseMatched(LeafList newDivided) {
+//		// if nothing divided then suppress chaseMatched messages
+//		if (newDivided.size()==0) return;
+//		
+//		globals.status2("------ starting chaseMatched");
 //		int i=0;
 //		while (newDivided.size()!=0) {
-//			globals.println("------ chaseDivided pass: " + i++);
-//			LeafList adjacent = StratAdjacent.doYourJob(newDivided, globals);
+//			globals.status2("------ chaseMatched pass: " + i++);
+//			LeafList newMatches = newDivided.selectMatched(globals);
+//			if (newMatches.size()==0) break;
+//			LeafList adjacent = StratAdjacent.doYourJob(newMatches, globals);
 //			if (adjacent.size()==0)  break;
 //			boolean doParts = isPartsList(adjacent);
-//			newDivided = doParts ? StratHashParts.doYourJob(adjacent, globals) :
-//								   StratHashWires.doYourJob(adjacent, globals);
+//			newDivided = 
+//				doParts ? StratHashParts.doYourJob(adjacent.iterator(), globals) :
+//					      StratHashWires.doYourJob(adjacent.iterator(), globals);
 //		}
-//		globals.println("------ done  chaseDivided after "+i+" passes");
-//		//		StratCheck.doYourJob(globals.getRoot(), globals);
-//		globals.println();
+//		globals.status2("------ done  chaseMatched after "+i+" passes");
+//		//		StratCheck.doYourJob(globals.getRoot, globals);
+//		globals.status2("");
 //	}
-	
-	private void hashFrontier() {
-		globals.status2("----- hash all NetObjects on frontier");
-		while (true) {
-			LeafList partOffspring = hashFrontierParts();
-			chaseMatched(partOffspring);
-		
-			LeafList wireOffspring = hashFrontierWires();
-			chaseMatched(wireOffspring);
-			if (partOffspring.size()==0 && wireOffspring.size()==0) break;
-		}
-	}
-	private boolean done() {
-//		LeafList p = StratFrontier.doYourJob(globals.getParts(),
-//		                                           globals);
-//		LeafList w = StratFrontier.doYourJob(globals.getWires(),
-//												   globals);
-		return globals.getWireLeafEquivRecs().numNotMatched()==0 &&
-			   globals.getPartLeafEquivRecs().numNotMatched()==0;
-	}
-	
-	private void useExportNames() {
-		globals.status2("----- use Export Names");
-		while (true) {
-			StratCount.doYourJob(globals);
-			LeafList offspring = StratPortName.doYourJob(globals);
-			if (offspring.size()==0) break;
-			chaseMatched(offspring);
-			hashFrontier();
-		}
-	}
-	private void useTransistorSizes() {
-		globals.status2("----- use transistor sizes");
-		while (true) {
-			LeafList offspring = StratSizes.doYourJob(globals);
-			if (offspring.size()==0) break;
-			chaseMatched(offspring);
-			hashFrontier();
-		}
-	}
-	
-	private void randomMatch() {
-		globals.status2("----- random matching");
-		while (true) {
-			LeafList offspring = StratRandomMatch.doYourJob(globals);
-			if (offspring.size()==0) break; 
-			chaseMatched(offspring);
-			hashFrontier();
-		}
-	}
-	
-	private void doWork() {
-		if (done()) return;
-		Date d1 = new Date();
-		
-		hashFrontier();
-		Date d2 = new Date();
-		globals.status1("  Hashing frontier took: "+NccUtils.hourMinSec(d1, d2));
-
-		if (done() || globals.userWantsToAbort()) return;
-		useExportNames();
-		Date d3 = new Date();
-		globals.status1("  Using export names took: "+NccUtils.hourMinSec(d2, d3));
-
-		if (done() || globals.userWantsToAbort()) return;
-		useTransistorSizes();
-		Date d4 = new Date();
-		globals.status1("  Using transistor sizes took: "+NccUtils.hourMinSec(d3, d4));
-
-		if (done() || globals.userWantsToAbort()) return;
-		randomMatch();
-		Date d5 = new Date();
-		globals.status1("  Random match took: "+NccUtils.hourMinSec(d4, d5));
-	}
-	
-	// contructor does all the work
-	private HashCodePartitioning(NccGlobals globals){
-		this.globals = globals;
-		globals.status2("----- starting HashCodePartitioning");
-		doWork();
-		globals.status2("----- done HashCodePartitioning");
-	}
-	
-	private boolean allPartsWiresMatch() {
-		return globals.getPartLeafEquivRecs().numNotMatched()==0 &&
-		       globals.getWireLeafEquivRecs().numNotMatched()==0;
-	}
-
-	// ------------------------ public methods --------------------------------
-	public static boolean doYourJob(NccGlobals globals) {
-		HashCodePartitioning p = new HashCodePartitioning(globals);
-		return p.allPartsWiresMatch();
-	}
-}
+//	
+////	/**
+////	 * Takes a list of newly divided Part/Wire EquivRecords. Finds
+////	 * their neighbors and performs a hash step on them to get newly
+////	 * divided Wire/Part EquivRecords. Repeat until the hash step
+////	 * yields no newly divided EquivRecords.
+////	 * @param newDivided newly divided EquivRecords
+////	 */
+////	private void chaseDivided(LeafList newDivided) {
+////		// if nothing divided then suppress chaseDivided messages
+////		if (newDivided.size()==0) return;
+////
+////		globals.println("------ starting chaseDivided");
+////		int i=0;
+////		while (newDivided.size()!=0) {
+////			globals.println("------ chaseDivided pass: " + i++);
+////			LeafList adjacent = StratAdjacent.doYourJob(newDivided, globals);
+////			if (adjacent.size()==0)  break;
+////			boolean doParts = isPartsList(adjacent);
+////			newDivided = doParts ? StratHashParts.doYourJob(adjacent, globals) :
+////								   StratHashWires.doYourJob(adjacent, globals);
+////		}
+////		globals.println("------ done  chaseDivided after "+i+" passes");
+////		//		StratCheck.doYourJob(globals.getRoot(), globals);
+////		globals.println();
+////	}
+//	
+//	private void hashFrontier() {
+//		globals.status2("----- hash all NetObjects on frontier");
+//		while (true) {
+//			LeafList partOffspring = hashFrontierParts();
+//			chaseMatched(partOffspring);
+//		
+//			LeafList wireOffspring = hashFrontierWires();
+//			chaseMatched(wireOffspring);
+//			if (partOffspring.size()==0 && wireOffspring.size()==0) break;
+//		}
+//	}
+//	private boolean done() {
+////		LeafList p = StratFrontier.doYourJob(globals.getParts(),
+////		                                           globals);
+////		LeafList w = StratFrontier.doYourJob(globals.getWires(),
+////												   globals);
+//		return globals.getWireLeafEquivRecs().numNotMatched()==0 &&
+//			   globals.getPartLeafEquivRecs().numNotMatched()==0;
+//	}
+//	
+//	private void useExportNames() {
+//		globals.status2("----- use Export Names");
+//		while (true) {
+//			StratCount.doYourJob(globals);
+//			LeafList offspring = StratPortName.doYourJob(globals);
+//			if (offspring.size()==0) break;
+//			chaseMatched(offspring);
+//			hashFrontier();
+//		}
+//	}
+//	private void useTransistorSizes() {
+//		globals.status2("----- use transistor sizes");
+//		while (true) {
+//			LeafList offspring = StratSizes.doYourJob(globals);
+//			if (offspring.size()==0) break;
+//			chaseMatched(offspring);
+//			hashFrontier();
+//		}
+//	}
+//	
+//	private void randomMatch() {
+//		globals.status2("----- random matching");
+//		while (true) {
+//			LeafList offspring = StratRandomMatch.doYourJob(globals);
+//			if (offspring.size()==0) break; 
+//			chaseMatched(offspring);
+//			hashFrontier();
+//		}
+//	}
+//	
+//	private void doWork() {
+//		if (done()) return;
+//		Date d1 = new Date();
+//		
+//		hashFrontier();
+//		Date d2 = new Date();
+//		globals.status1("  Hashing frontier took: "+NccUtils.hourMinSec(d1, d2));
+//
+//		if (done() || globals.userWantsToAbort()) return;
+//		useExportNames();
+//		Date d3 = new Date();
+//		globals.status1("  Using export names took: "+NccUtils.hourMinSec(d2, d3));
+//
+//		if (done() || globals.userWantsToAbort()) return;
+//		useTransistorSizes();
+//		Date d4 = new Date();
+//		globals.status1("  Using transistor sizes took: "+NccUtils.hourMinSec(d3, d4));
+//
+//		if (done() || globals.userWantsToAbort()) return;
+//		randomMatch();
+//		Date d5 = new Date();
+//		globals.status1("  Random match took: "+NccUtils.hourMinSec(d4, d5));
+//	}
+//	
+//	// contructor does all the work
+//	private HashCodePartitioning(NccGlobals globals){
+//		this.globals = globals;
+//		globals.status2("----- starting HashCodePartitioning");
+//		doWork();
+//		globals.status2("----- done HashCodePartitioning");
+//	}
+//	
+//	private boolean allPartsWiresMatch() {
+//		return globals.getPartLeafEquivRecs().numNotMatched()==0 &&
+//		       globals.getWireLeafEquivRecs().numNotMatched()==0;
+//	}
+//
+//	// ------------------------ public methods --------------------------------
+//	public static boolean doYourJob(NccGlobals globals) {
+//		HashCodePartitioning p = new HashCodePartitioning(globals);
+//		return p.allPartsWiresMatch();
+//	}
+//}
--- srcj/com/sun/electric/tool/ncc/strategy/StratAdjacent.java
+++ srcj/com/sun/electric/tool/ncc/strategy/StratAdjacent.java
@@ -21,91 +21,91 @@
  * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
  * Boston, Mass 02111-1307, USA.
  */
-package com.sun.electric.tool.ncc.strategy;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-
-import com.sun.electric.tool.ncc.NccGlobals;
-import com.sun.electric.tool.ncc.lists.LeafList;
-import com.sun.electric.tool.ncc.lists.RecordList;
-import com.sun.electric.tool.ncc.netlist.NetObject;
-import com.sun.electric.tool.ncc.trees.Circuit;
-import com.sun.electric.tool.ncc.trees.EquivRecord;
-
-/** StratAdjacent returns a list of the leaf EquivRecords adjacent to the
- * input list or record */
-public class StratAdjacent extends Strategy {
-	private int numEquivProcessed;
-	private Set<EquivRecord> adjacent = new HashSet<EquivRecord>();
-
-	private StratAdjacent(NccGlobals globals) {super(globals);}
-	
-	public static LeafList doYourJob(RecordList l, 
-									     NccGlobals globals) {
-		StratAdjacent ja = new StratAdjacent(globals);
-		return ja.doFor(l);
-	}
-	
-	//do something before starting
-	private void preamble(RecordList j){
-//		startTime("StratAdjacent", "a list of " + j.size());
-	}
-	
-    //summarize at the end
-    private LeafList summary(){
-		LeafList offspring = new LeafList();
-		for (EquivRecord er : adjacent) {
-			offspring.add(er);
-		}
-
-//		globals.println("StratAdjacent done - processed " +
-//							numEquivProcessed + " EquivRecords");
-
-		globals.status2(" StratAdjacent"+offspringStats(offspring));
-
-//		elapsedTime();
-		return offspring.selectActive(globals);
-    }
-	
-    public LeafList doFor(RecordList g){
-		preamble(g);
-		super.doFor(g);
-        return summary();
-    }
-	
-	/** add EquivRecords that are adjacent to the contents of er. A
-	 * EquivRecord is adjacent if it can be reached in one step
-	 * from any of er's circuits and it's live and not matched nor
-	 * mismatched
-	 * @return Set of adjacent EquivRecords */
-	private void addAdjacentEquivRecs(EquivRecord er){
-		for (Iterator<Circuit> ci=er.getCircuits(); ci.hasNext();) {
-			Circuit jc= ci.next();
-			for (Iterator<NetObject> ni=jc.getNetObjs(); ni.hasNext();) {
-				NetObject netObj= ni.next();
-				for (Iterator<NetObject> it=netObj.getConnected(); it.hasNext();) {
-					//for each adjacent NetObject
-					netObj = it.next();
-					EquivRecord sg = netObj.getParent().getParent();
-					if(sg.isActive()) adjacent.add(sg);
-				}
-			}
-		}
-	}
-	
-	/** The real output of this method is what it adds to adjacent */
-    public LeafList doFor(EquivRecord g){
-		if(g.isLeaf()) {
-			numEquivProcessed++;
-			addAdjacentEquivRecs(g);
-//			String s= ("processed " + g.nameString());
-//			globals.println(s + " to find " + out.size() + " adjacent");
-		} else {
-			globals.status2("processing " + g.nameString());
-			super.doFor(g);
-		}
-		return new LeafList(); // return value is ignored
-	}
-	
-}
+//package com.sun.electric.tool.ncc.strategy;
+//import java.util.HashSet;
+//import java.util.Iterator;
+//import java.util.Set;
+//
+//import com.sun.electric.tool.ncc.NccGlobals;
+//import com.sun.electric.tool.ncc.lists.LeafList;
+//import com.sun.electric.tool.ncc.lists.RecordList;
+//import com.sun.electric.tool.ncc.netlist.NetObject;
+//import com.sun.electric.tool.ncc.trees.Circuit;
+//import com.sun.electric.tool.ncc.trees.EquivRecord;
+//
+///** StratAdjacent returns a list of the leaf EquivRecords adjacent to the
+// * input list or record */
+//public class StratAdjacent extends Strategy {
+//	private int numEquivProcessed;
+//	private Set<EquivRecord> adjacent = new HashSet<EquivRecord>();
+//
+//	private StratAdjacent(NccGlobals globals) {super(globals);}
+//	
+//	public static LeafList doYourJob(RecordList l, 
+//									     NccGlobals globals) {
+//		StratAdjacent ja = new StratAdjacent(globals);
+//		return ja.doFor(l);
+//	}
+//	
+//	//do something before starting
+//	private void preamble(RecordList j){
+////		startTime("StratAdjacent", "a list of " + j.size());
+//	}
+//	
+//    //summarize at the end
+//    private LeafList summary(){
+//		LeafList offspring = new LeafList();
+//		for (EquivRecord er : adjacent) {
+//			offspring.add(er);
+//		}
+//
+////		globals.println("StratAdjacent done - processed " +
+////							numEquivProcessed + " EquivRecords");
+//
+//		globals.status2(" StratAdjacent"+offspringStats(offspring));
+//
+////		elapsedTime();
+//		return offspring.selectActive(globals);
+//    }
+//	
+//    public LeafList doFor(RecordList g){
+//		preamble(g);
+//		super.doFor(g);
+//        return summary();
+//    }
+//	
+//	/** add EquivRecords that are adjacent to the contents of er. A
+//	 * EquivRecord is adjacent if it can be reached in one step
+//	 * from any of er's circuits and it's live and not matched nor
+//	 * mismatched
+//	 * @return Set of adjacent EquivRecords */
+//	private void addAdjacentEquivRecs(EquivRecord er){
+//		for (Iterator<Circuit> ci=er.getCircuits(); ci.hasNext();) {
+//			Circuit jc= ci.next();
+//			for (Iterator<NetObject> ni=jc.getNetObjs(); ni.hasNext();) {
+//				NetObject netObj= ni.next();
+//				for (Iterator<NetObject> it=netObj.getConnected(); it.hasNext();) {
+//					//for each adjacent NetObject
+//					netObj = it.next();
+//					EquivRecord sg = netObj.getParent().getParent();
+//					if(sg.isActive()) adjacent.add(sg);
+//				}
+//			}
+//		}
+//	}
+//	
+//	/** The real output of this method is what it adds to adjacent */
+//    public LeafList doFor(EquivRecord g){
+//		if(g.isLeaf()) {
+//			numEquivProcessed++;
+//			addAdjacentEquivRecs(g);
+////			String s= ("processed " + g.nameString());
+////			globals.println(s + " to find " + out.size() + " adjacent");
+//		} else {
+//			globals.status2("processing " + g.nameString());
+//			super.doFor(g);
+//		}
+//		return new LeafList(); // return value is ignored
+//	}
+//	
+//}
