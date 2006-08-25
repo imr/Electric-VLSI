21a0f37320ec194cc41adcaba2c9d40671badbe8
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2006-08-25 06:39:58 +0000

    backup
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@5136 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/extract/TransistorSearch.java
+++ srcj/com/sun/electric/tool/extract/TransistorSearch.java
@@ -5,8 +5,15 @@ import com.sun.electric.database.hierarchy.Nodable;
 import com.sun.electric.database.hierarchy.Cell;
 import com.sun.electric.database.hierarchy.View;
 import com.sun.electric.database.topology.NodeInst;
+import com.sun.electric.database.topology.PortInst;
 import com.sun.electric.database.variable.VarContext;
-import com.sun.electric.technology.PrimitiveNode;
+import com.sun.electric.database.network.Network;
+import com.sun.electric.database.network.Netlist;
+import com.sun.electric.database.network.Global;
+import com.sun.electric.database.prototype.PortCharacteristic;
+
+import java.util.Iterator;
+import java.util.HashMap;
 
 /**
  * Created by IntelliJ IDEA.
@@ -49,8 +56,37 @@ public class TransistorSearch
             if (ni.isCellInstance()) return true;
             if (ni.getProto().getFunction().isTransistor())
             {
+                Netlist netlist = info.getNetlist();
+                Global.Set globals = netlist.getGlobals();
                 // checking the ports
-                transistorNumber++;
+                boolean found = false; // no cap transistor (2 gnd or 2 vdd)
+                HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+                int netID = -1;
+                for (Iterator<PortInst> itPi = ni.getPortInsts(); itPi.hasNext();)
+                {
+                    PortInst pi = itPi.next();
+                    // Only checking active ports: source and drain
+                    if (pi.getPortProto().getCharacteristic() != PortCharacteristic.BIDIR)
+                        continue;
+                    Network net = netlist.getNetwork(pi);
+                    int key = info.getNetID(net);
+                    if (netID == -1)
+                        netID = key;
+                    else if (key == netID)  // same network
+                    {
+                        found = true;
+                        for (int j = 0; j < globals.size(); j++)
+                        {
+                            Global g = globals.get(j);
+                            if (netlist.getNetwork(g) == net)
+                                System.out.println();
+                        }
+                        break;
+                    }
+                }
+                // Only counting when it is not a cap transistor.
+                if (!found)
+                    transistorNumber++;
             }
             return true;
         }
