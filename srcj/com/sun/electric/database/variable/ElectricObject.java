73ba0929c5203b9b5e54b53240f6b690424003c7
strubin <strubin@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2006-09-27 20:51:58 +0000

    Fixed multiline text on rotated nodes
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@5396 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/variable/ElectricObject.java
+++ srcj/com/sun/electric/database/variable/ElectricObject.java
@@ -474,9 +474,15 @@ public abstract class ElectricObject implements Serializable
 			Poly.Type rotStyle = style;
 			if (this instanceof NodeInst)
 			{
+				NodeInst ni = (NodeInst)this;
+				AffineTransform offsetTrans = ni.pureRotateIn();
+				Point2D off = new Point2D.Double(lineOffX, lineOffY);
+				offsetTrans.transform(off, off);
+				lineOffX = off.getX();
+				lineOffY = off.getY();
+
 				if (style != Poly.Type.TEXTCENT && style != Poly.Type.TEXTBOX)
 				{
-					NodeInst ni = (NodeInst)this;
 					trans = ni.rotateIn();
 					int origAngle = style.getTextAngle();
 					if (ni.isMirroredAboutXAxis() != ni.isMirroredAboutYAxis() &&
