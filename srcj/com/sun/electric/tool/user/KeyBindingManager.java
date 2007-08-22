e36a5e82a53834f1fb148656c8d90c539a1a2c73
strubin <strubin@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2007-08-22 17:22:27 +0000

    Fixed two-key bindings (Bug #980)
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@6503 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/user/KeyBindingManager.java
+++ srcj/com/sun/electric/tool/user/KeyBindingManager.java
@@ -32,7 +32,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.InputEvent;
 import java.awt.event.KeyEvent;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.prefs.Preferences;
 
 import javax.swing.AbstractAction;
