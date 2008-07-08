9481a495b17c81df40596001ca481b2cdf755b47
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2008-07-08 23:51:24 +0000

    wrong commit
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@7838 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/text/Version.java
+++ srcj/com/sun/electric/database/text/Version.java
@@ -59,7 +59,7 @@ public class Version implements Comparable<Version>, Serializable
 	/**
 	 * This is the current version of Electric
 	 */
-	private static final String CURRENT = "8.07a";
+	private static final String CURRENT = "8.08a";
 	private static final String ROOTARNAME = "electric";
 
 	private final String version;
