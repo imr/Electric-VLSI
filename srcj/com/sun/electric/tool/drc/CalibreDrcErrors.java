771a4265657034a5c303f54b68d70f245adc8c58
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2008-09-29 22:56:05 +0000

    The right solution of Bug #1888
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@8248 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/drc/CalibreDrcErrors.java
+++ srcj/com/sun/electric/tool/drc/CalibreDrcErrors.java
@@ -144,9 +144,8 @@ public class CalibreDrcErrors {
             while(true) {
                 DrcRuleViolation v = readRule();
                 if (v == null) break;
-                if (v.errors.size() == 0) // nothing to include
-                    break;
-                ruleViolations.add(v);
+                if (v.errors.size() != 0) // something to include
+                    ruleViolations.add(v);
             }
         } catch (IOException e) {
             System.out.println("Error reading file: "+e.getMessage());
