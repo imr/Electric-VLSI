4f9b132c3cf8de30129eb0cc1b562bf94b7e6793
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2009-01-12 23:44:33 +0000

    Fixing TechEditorWizard regression due to year change. v8.09b
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@8639 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/text/Version.java
+++ srcj/com/sun/electric/database/text/Version.java
@@ -59,7 +59,7 @@ public class Version implements Comparable<Version>, Serializable
 	/**
 	 * This is the current version of Electric
 	 */
-	private static final String CURRENT = "8.09a";
+	private static final String CURRENT = "8.09b";
 	private static final String ROOTARNAME = "electric";
 
 	private final String version;
