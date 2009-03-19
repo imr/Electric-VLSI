0f0e45f53d0214088d296494dc18bb4c04b98a5d
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2009-03-19 20:26:32 +0000

    v8.09g
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@9019 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/text/Version.java
+++ srcj/com/sun/electric/database/text/Version.java
@@ -59,7 +59,7 @@ public class Version implements Comparable<Version>, Serializable
 	/**
 	 * This is the current version of Electric
 	 */
-	private static final String CURRENT = "8.09f";
+	private static final String CURRENT = "8.09g";
 	private static final String ROOTARNAME = "electric";
 
 	private final String version;
