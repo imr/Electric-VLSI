8a30144f890baf983a273b7329b690ce1181fde8
gg151869 <gg151869@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2009-07-06 20:37:05 +0000

    BUG: (7/6/09, GVG) All *.  lay cells renamed to deleted after running the stitch generator? (Bug #2272) FIX: Undone in Output.writeLibrary() for bug #2256.
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@9695 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/io/output/Output.java
+++ srcj/com/sun/electric/tool/io/output/Output.java
@@ -390,18 +390,12 @@ public class Output
 			libFile = TextUtils.makeURLToFile(lib.getName());
 
 		// make the proper output file name
-		String fullOutputName = TextUtils.URLtoString(libFile);
 		String properOutputNameWithoutExtension = TextUtils.getFilePath(libFile) + TextUtils.getFileNameWithoutExtension(libFile);
-		String properOutputName = fullOutputName;
-
-		// add an extension if there is none
-		if (properOutputNameWithoutExtension.equals(fullOutputName))
-		{
-			if (type == FileType.ELIB) properOutputName = properOutputNameWithoutExtension + ".elib";
-			if (type == FileType.JELIB) properOutputName = properOutputNameWithoutExtension + ".jelib";
-			if (type == FileType.DELIB) properOutputName = properOutputNameWithoutExtension + ".delib";
-			if (type == FileType.READABLEDUMP) properOutputName = properOutputNameWithoutExtension + ".txt";
-		}
+		String properOutputName = properOutputNameWithoutExtension;
+		if (type == FileType.ELIB) properOutputName += ".elib";
+		if (type == FileType.JELIB) properOutputName += ".jelib";
+		if (type == FileType.DELIB) properOutputName += ".delib";
+		if (type == FileType.READABLEDUMP) properOutputName += ".txt";
 		if (type == FileType.ELIB || type == FileType.JELIB || type == FileType.DELIB)
 		{
 			// backup previous files if requested
