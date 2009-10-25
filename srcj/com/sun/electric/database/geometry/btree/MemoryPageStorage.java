b90aca689b836634c33b2adcef21d2d02e000086
am77536 <am77536@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2009-10-25 04:28:13 +0000

    add MemoryPageStorage
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@10279 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ srcj/com/sun/electric/database/geometry/btree/MemoryPageStorage.java
@@ -0,0 +1,59 @@
+/* -*- tab-width: 4 -*-
+ *
+ * Electric(tm) VLSI Design System
+ *
+ * File: MemoryPageStorage.java
+ *
+ * Copyright (c) 2009 Sun Microsystems and Static Free Software
+ *
+ * Electric(tm) is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Electric(tm) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Electric(tm); see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, Mass 02111-1307, USA.
+ */
+package com.sun.electric.database.geometry.btree;
+
+/** A PageStorage that uses plain old memory. */
+public class MemoryPageStorage extends PageStorage {
+
+    private       byte[][] pages;
+    private       int      numpages;
+    private final int      pagesize;
+
+    public MemoryPageStorage(int pagesize) {
+        this.pagesize = pagesize;
+        this.numpages = 0;
+        this.pages = new byte[0][];
+    }
+
+    public int getPageSize() { return pagesize; }
+    public int getNumPages() { return numpages; }
+    public int createPage() {
+        if (numpages >= pages.length) {
+            byte[][] newpages = new byte[pages.length*2][];
+            System.arraycopy(pages, 0, newpages, 0, pages.length);
+            pages = newpages;
+        }
+        pages[numpages] = new byte[pagesize];
+        return numpages++;
+    }
+    public void flush(int pageid) { }
+    public void flush() { }
+    public void writePage(int pageid, byte[] buf, int ofs) {
+        System.arraycopy(buf, ofs, pages[pageid], 0, pagesize);
+    }
+    public byte[] readPage(int pageid, byte[] buf, int ofs) {
+        System.arraycopy(pages[pageid], 0, buf, ofs, pagesize);
+        return buf;
+    }
+}
