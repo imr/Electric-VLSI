316fe3098209b1073887e630dfca747783756c37
am77536 <am77536@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-02-25 06:46:30 +0000

    IMP: (2/24/10, AM) Add RangeSample.
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@10923 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ srcj/com/sun/electric/tool/simulation/RangeSample.java
@@ -0,0 +1,57 @@
+/* -*- tab-width: 4 -*-
+ *
+ * Electric(tm) VLSI Design System
+ *
+ * File: Signal.java
+ *
+ * Copyright (c) 2010 Sun Microsystems and Static Free Software
+ *
+ * Electric(tm) is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Electric(tm) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Electric(tm); see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, Mass 02111-1307, USA.
+ */
+package com.sun.electric.tool.simulation;
+import java.io.*;
+
+/**
+ *  A sample consisting of a range (a min and a max).  Example usage:
+ *  a Signal<RangeSample<S>> gives the bounds of a Signal<S> over a
+ *  period of time.  Signal.View<RangeSample<S>> is a very useful type.
+ */
+public class RangeSample<S extends Sample & Comparable> implements Sample {
+
+    private final S min;
+    private final S max;
+
+    public RangeSample(S min, S max) { this.min = min; this.max = max; }
+
+    public S getMin() { return min; }
+    public S getMax() { return max; }
+
+    public boolean equals(Object o) {
+        if (o==null || !(o instanceof RangeSample)) return false;
+        RangeSample rs = (RangeSample)o;
+        return rs.min.equals(min) && rs.max.equals(max);
+    }
+
+    public int hashCode() {
+        return min.hashCode() ^ max.hashCode();
+    }
+
+    public boolean isLogicX() { return false; }
+    public boolean isLogicZ() { return false; }
+
+}
+
+
