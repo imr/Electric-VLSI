7e3139061a798e2e6704d7223fb4c7b633984458
am77536 <am77536@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-06-11 00:26:02 +0000

    IMP: (6/10/10, AM) Stub for PageStorageWithDeallocation.
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@11323 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ srcj/com/sun/electric/database/geometry/btree/PageStorageWithDeallocation.java
@@ -0,0 +1,39 @@
+/* -*- tab-width: 4 -*-
+ *
+ * Electric(tm) VLSI Design System
+ *
+ * File: BTree.java
+ *
+ * Copyright (c) 2009 Sun Microsystems and Static Free Software
+ *
+ * Electric(tm) is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Electric(tm) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Electric(tm); see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, Mass 02111-1307, USA.
+ */
+package com.sun.electric.database.geometry.btree;
+
+import java.io.*;
+
+/**
+ *  A PageStorage that can deallocate pages -- all pages with a larger
+ *  page number are instantaneously renumbered!  Users must beware of
+ *  this.
+ */
+public interface PageStorageWithDeallocation {
+
+    // TO DO: provide an upcall to notify users that renumbering has occurred
+
+    public void discardPage(int page);
+
+}
