c086a28afdd8d7c9965d015703101965a3a337b3
am77536 <am77536@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-06-11 00:26:12 +0000

    IMP: (6/10/10, AM) RadixTrie improvements (incomplete).
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@11324 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/database/geometry/btree/RadixTrie.java
+++ srcj/com/sun/electric/database/geometry/btree/RadixTrie.java
@@ -33,35 +33,128 @@ import java.util.*;
  *
  *  A RadixTrie consists of a tree of nodes; each path from the root
  *  to a node with entry!=null constitutes an entry.  The
- *  concatenation of all label fields along the path is the key and
+ *  concatenation of all TreeMap keys along the path is the key and
  *  the entry value is the entry.  So you might think of a RadixTrie
  *  as a Map<String,V> which has excellent space efficiency when the
  *  keys share large prefixes.
  *
  *  An additional feature in this implementation (not normally
  *  included in RadixTries) is the tracking of each character's
- *  "score".  The score of a node is defined to be the number of
- *  descendents it has with entry!=null.  The score of a character "c"
- *  is the sum of the scores of all nodes for which
- *  label.charAt(label.length()-1)==c.  The character with the highest
- *  score is a very good guess at the "hierarchy separator"; it is the
- *  character which is most likely to terminate the longest common
- *  prefix of a family of strings.
+ *  "score".  The score of a branch is defined to be the number of
+ *  descendents it has with entry!=null.  The score of a node is the
+ *  *second largest* score among its branches.  The score of a
+ *  character "c" is the sum of the scores of all nodes "n" such that
+ *  the last charachter of its key (in its parent's TreeMap) is "c".
+ *  The character with the highest score is a very good guess at the
+ *  "hierarchy separator"; it is the character which is most likely to
+ *  terminate the longest common prefix of a family of strings.
  *
  *  @author Adam Megacz <adam.megacz@oracle.com>
  */
 public class RadixTrie<V> {
 
     // the root node
-    private Node root = null;
+    private final Node root = new Node((char)0, null);
+
+    private final int[] scores = new int[65535];
 
     private class Node {
-        // if this!=root then label.length()!=0
-        String label;
 
-        // invariant: for any character "c", children.get(c).label.charAt(0)==c
-        HashMap<Character,Node> children = null;
+        private char firstChar;
+        private V entry;
 
-        V entry;
+        private TreeMap<String,Node> children = null;
+
+        public Node(char firstChar, V value) {
+            this.firstChar = firstChar;
+            this.entry = value;
+        }
+
+        private String getPrev(String key) {
+            if (children==null) return null;
+            SortedMap<String,Node> prevMap = children.headMap(key);
+            return prevMap.size()==0 ? null : prevMap.lastKey();
+        }
+
+        public void put(String key, V value) {
+            if (key.length()!=0 && children==null) children = new TreeMap<String,Node>();
+
+            // prev is the key right before the "insertion point"
+            String prev = getPrev(key);
+
+            if (key.length()==0) {
+                this.entry = value;
+
+            } else if (children.containsKey(key)) {
+                children.get(key).put("", value);
+
+            } else if (prev!=null && key.startsWith(prev)) {
+                // if prev is a prefix of the key we're inserting, we can just walk down that branch
+                children.get(prev).put(key.substring(prev.length()), value);
+                    
+            } else if (prev==null || prev.charAt(0) != key.charAt(0)) {
+                // if prev shares not even a single character, we need to make a new branch
+                if (children==null) children = new TreeMap<String,Node>();
+                children.put(key, new Node(key.charAt(0), value));
+                scores[firstChar & 0xffff]++;
+
+            } else {
+                // prev shares more than one but less than all characters; need to split a branch
+                int i=0;
+                for(i=0; i<Math.min(key.length(), prev.length()); i++)
+                    if (key.charAt(i)!=prev.charAt(i))
+                        break;
+
+                Node oldnode = children.get(prev);
+                children.remove(prev);
+                scores[firstChar & 0xffff]--;
+
+                Node newnode = new Node(key.charAt(i-1), null);
+                children.put(key.substring(0, i), newnode);
+                scores[firstChar & 0xffff]++;
+
+                if (newnode.children==null) newnode.children = new TreeMap<String,Node>();
+                newnode.children.put(key.substring(i), new Node(key.charAt(key.length()-1), value));
+                scores[newnode.firstChar & 0xffff]++;
+                newnode.children.put(prev.substring(i), oldnode);
+                scores[newnode.firstChar & 0xffff]++;
+            }
+        }
+        private int size() { return children==null ? 0 : children.size(); }
+        public V get(String key) {
+            if (key.length()==0) return entry;
+            if (children==null) return null;
+            if (children.containsKey(key)) return children.get(key).get("");
+            SortedMap<String,Node> prevMap = children.headMap(key);
+            String prev = prevMap.size()==0 ? null : prevMap.lastKey();
+            if (prev!=null && key.startsWith(prev)) return children.get(prev).get(key.substring(prev.length()));
+            return null;
+        }
+
     }
+
+
+    public char guessHierarchySeparator() {
+        int best = 0;
+        char ret = 0;
+        for(int i=0; i<scores.length; i++) {
+            if (scores[i] > best) {
+                best = scores[i];
+                ret = (char)i;
+            }
+        }
+        return ret;
+    }
+
+    public V    get(String key) {
+        return root.get(key);
+    }
+
+    public void put(String key, V value) {
+        System.out.println("put \""+key+"\"");
+        if (value==null) throw new RuntimeException("deletions not yet implemented");
+        root.put(key, value);
+    }
+
+
 }
