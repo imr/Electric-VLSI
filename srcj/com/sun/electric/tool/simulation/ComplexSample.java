ac7be9003a615f038436f4a6c23cf3756f8ffbf1
am77536 <am77536@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-06-19 22:04:48 +0000

    implement a few more methods in ComplexSample.java
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@11416 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- srcj/com/sun/electric/tool/simulation/ComplexSample.java
+++ srcj/com/sun/electric/tool/simulation/ComplexSample.java
@@ -23,6 +23,8 @@
  */
 package com.sun.electric.tool.simulation;
 import java.io.*;
+import com.sun.electric.database.geometry.btree.*;
+import com.sun.electric.database.geometry.btree.unboxed.*;
 
 /**
  *  An implementation of Sample for complex data.  Holds 
@@ -55,7 +57,8 @@ public class ComplexSample implements Sample {
     public boolean isLogicZ() { return false; }
 
     public Sample lub(Sample s) {
-        if (!(s instanceof ComplexSample)) throw new RuntimeException("tried to call ComplexSample.lub("+s.getClass().getName()+")");
+        if (!(s instanceof ComplexSample))
+            throw new RuntimeException("tried to call ComplexSample.lub("+s.getClass().getName()+")");
         ComplexSample cs = (ComplexSample)s;
         if (cs.real >= real && cs.imag >= imag) return cs;
         if (cs.real <= real && cs.imag <= imag) return this;
@@ -63,11 +66,53 @@ public class ComplexSample implements Sample {
     }
 
     public Sample glb(Sample s) {
-        if (!(s instanceof ComplexSample)) throw new RuntimeException("tried to call ComplexSample.glb("+s.getClass().getName()+")");
+        if (!(s instanceof ComplexSample))
+            throw new RuntimeException("tried to call ComplexSample.glb("+s.getClass().getName()+")");
         ComplexSample cs = (ComplexSample)s;
         if (cs.real >= real && cs.imag >= imag) return this;
         if (cs.real <= real && cs.imag <= imag) return cs;
         return new ComplexSample(Math.min(real, cs.real), Math.min(imag, cs.imag));
+    }
+
+    public static final Unboxed<ComplexSample> unboxer = new Unboxed<ComplexSample>() {
+        public int getSize() { return UnboxedHalfDouble.instance.getSize()*2; }
+        public ComplexSample deserialize(byte[] buf, int ofs) {
+            return new ComplexSample(UnboxedHalfDouble.instance.deserialize(buf, ofs),
+                                     UnboxedHalfDouble.instance.deserialize(buf, ofs+UnboxedHalfDouble.instance.getSize()));
+        }
+        public void serialize(ComplexSample v, byte[] buf, int ofs) {
+            UnboxedHalfDouble.instance.serialize(v.real, buf, ofs);
+            UnboxedHalfDouble.instance.serialize(v.imag, buf, ofs+UnboxedHalfDouble.instance.getSize());
+        }
+    };
+
+    /*
+                        double val = Math.hypot(imagPart, realPart);
+                        if (signal.getSample(time)==null)
+                            signal.addSample(time, new ScalarSample(val));
+    */
+
+    private static final LatticeOperation<ComplexSample> latticeOp =
+        new LatticeOperation<ComplexSample>(unboxer) {
+        private final int sz = UnboxedHalfDouble.instance.getSize();
+        public void glb(byte[] buf1, int ofs1, byte[] buf2, int ofs2, byte[] dest, int dest_ofs) {
+            ScalarSample.latticeOp.glb(buf1, ofs1,    buf2, ofs2,    dest, dest_ofs);
+            ScalarSample.latticeOp.glb(buf1, ofs1+sz, buf2, ofs2+sz, dest, dest_ofs+sz);
+        }
+        public void lub(byte[] buf1, int ofs1, byte[] buf2, int ofs2, byte[] dest, int dest_ofs) {
+            ScalarSample.latticeOp.lub(buf1, ofs1,    buf2, ofs2,    dest, dest_ofs);
+            ScalarSample.latticeOp.lub(buf1, ofs1+sz, buf2, ofs2+sz, dest, dest_ofs+sz);
+        }
+    };
+
+    public static Signal<ComplexSample> createSignal(Analysis an, String signalName, String signalContext) {
+        Signal<ComplexSample> ret =
+            new BTreeSignal<ComplexSample>(an, signalName, signalContext, BTreeSignal.getTree(unboxer, latticeOp)) {
+            public boolean isDigital() { return false; }
+            public boolean isAnalog() { return true; }
+        };
+        an.addSignal(ret);
+        return ret;
     }
 
 }
