ce722c81e4e033d8b1c03c2ebf9cf305276ddfaf
fs239085 <fs239085@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-08-23 12:13:26 +0000

    AbstractJunitTest - load lib + load cell
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@11719 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ tests/com/sun/electric/tool/util/test/AbstractJunitTest_T.java
@@ -0,0 +1,51 @@
+/* -*- tab-width: 4 -*-
+ *
+ * Electric(tm) VLSI Design System
+ *
+ * File: AbstractJunitTest_T.java
+ *
+ * Copyright (c) 2010 Sun Microsystems and Static Free Software
+ *
+ * Electric(tm) is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Electric(tm) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Electric(tm); see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, Mass 02111-1307, USA.
+ */
+package com.sun.electric.tool.util.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.sun.electric.database.hierarchy.Cell;
+import com.sun.electric.database.hierarchy.Library;
+
+/**
+ * @author Felix Schmidt
+ * 
+ */
+public class AbstractJunitTest_T extends AbstractJunitTest {
+
+	@Test
+	public void testLoadLibrary() throws Exception {
+		Library lib = this.loadLibrary("PlacementTest", "W:/workspace/regression/tools/Placement/data/libs/placementTests.jelib");
+
+		Assert.assertNotNull(lib);
+	}
+	
+	@Test
+	public void testLoadCell() throws Exception {
+		Cell cell = this.loadCell("PlacementTest", "PlacementTest4", "W:/workspace/regression/tools/Placement/data/libs/placementTests.jelib");
+		Assert.assertNotNull(cell);
+	}
+
+}
