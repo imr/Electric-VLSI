2c7b3c753fd7d5470fcd9c5feb40ae2ed8534835
dn146861 <dn146861@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-08-27 03:59:00 +0000

    ScalaProject for DRC min area check
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@11750 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ ScalaProjects/AreaCheck/src/com/sun/electric/scala/Main.scala
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.sun.electric.scala
+
+object Main {
+
+  /**
+   * @param args the command line arguments
+   */
+  def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+    com.sun.electric.Launcher.main(args)
+  }
+
+}
