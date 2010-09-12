21a218b95ea7b41f25c980ea3d917ddadfd309e8
dn146861 <dn146861@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-09-12 17:59:33 +0000

    Modify mkmaven.sh
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@11917 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- packaging/mkmaven.sh
+++ packaging/mkmaven.sh
@@ -5,6 +5,6 @@
 #    mkmaven
 
 #mvn install:install-file -Dfile=jmf.jar -DgroupId=javax.media -DartifactId=jmf -Dversion=2.1.1e -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=prefuse.jar -DgroupId=org.prefuse -DartifactId=prefuse -Dversion=beta-20060809 -Dpackaging=jar -DgeneratePom=true
+#mvn install:install-file -Dfile=prefuse.jar -DgroupId=org.prefuse -DartifactId=prefuse -Dversion=beta-20060809 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=AppleJavaExtensions.jar -DgroupId=com.apple -DartifactId=AppleJavaExtensions -Dversion=1.3 -Dpackaging=jar -DgeneratePom=true
 
