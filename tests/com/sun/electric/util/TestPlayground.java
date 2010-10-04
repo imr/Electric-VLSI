79d6bbff9aa8e08f7f1f7579cb4e8b51c1cba7f8
fs239085 <fs239085@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-10-04 07:48:22 +0000

    merge trunk & maven branches: test playground
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@12099 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ tests/com/sun/electric/util/TestPlayground.java
@@ -0,0 +1,12 @@
+package com.sun.electric.util;
+
+import org.junit.Test;
+
+public class TestPlayground {
+    
+    @Test
+    public void testUserHome() {
+       System.out.println(System.getProperty("user.home")); 
+    }
+
+}
