44fa037af48988520001f891d09d4a7394a67c54
dn146861 <dn146861@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-10-04 07:03:19 +0000

    Fake repository in packaging
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@12090 b64ed6c1-75a8-e629-b1da-d0dae74a743a

--- packaging/mkmaven.sh
+++ packaging/mkmaven.sh
@@ -4,7 +4,8 @@
 # usage:
 #    mkmaven
 
-mvn install:install-file -Dfile=jmf.jar -DgroupId=javax.media -DartifactId=jmf -Dversion=2.1.1e -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=prefuse.jar -DgroupId=org.prefuse -DartifactId=prefuse -Dversion=beta-20060809 -Dpackaging=jar -DgeneratePom=true
-mvn install:install-file -Dfile=AppleJavaExtensions.jar -DgroupId=com.apple -DartifactId=AppleJavaExtensions -Dversion=1.4 -Dpackaging=jar -DgeneratePom=true
+REPO=.m2/repository/
+mvn install:install-file -Dfile=${REPO}javax/media/2.1.1e/jmf-2.1.1e.jar -DgroupId=javax.media -DartifactId=jmf -Dversion=2.1.1e -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=${REPO}org/prefuse/prefuse/beta-20060809/prefuse-beta-20060809.jar -DgroupId=org.prefuse -DartifactId=prefuse -Dversion=beta-20060809 -Dpackaging=jar -DgeneratePom=true
+mvn install:install-file -Dfile=${REPO}com/apple/AppleJavaExtensions/1.4/AppleJavaExtensions-1.4.jar -DgroupId=com.apple -DartifactId=AppleJavaExtensions -Dversion=1.4 -Dpackaging=jar -DgeneratePom=true
 
