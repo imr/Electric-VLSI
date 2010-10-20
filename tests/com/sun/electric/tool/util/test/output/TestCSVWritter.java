27be3a702d869710b13e691d60061ed30a2914d9
fs239085 <fs239085@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-10-20 07:51:17 +0000

    remove unused packages
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@12347 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ tests/com/sun/electric/tool/util/test/output/TestCSVWritter.java
@@ -0,0 +1,58 @@
+/* -*- tab-width: 4 -*-
+ *
+ * Electric(tm) VLSI Design System
+ *
+ * File: TestCSVWritter.java
+ *
+ * Copyright (c) 2010 Sun Microsystems and Static Free Software
+ *
+ * Electric(tm) is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Electric(tm) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Electric(tm); see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ * Boston, Mass 02111-1307, USA.
+ */
+package com.sun.electric.tool.util.test.output;
+
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+
+import com.sun.electric.tool.io.output.CSVWriter;
+import com.sun.electric.util.test.TestHelpers;
+
+/**
+ * @author Schmidt
+ * 
+ */
+public class TestCSVWritter {
+
+    @Ignore
+    @Test
+    public void testCreateLineEntry() throws Exception {
+//        CSVWriter writer = new CSVWriter(new File("test.csv"), ',');
+//
+//        List<String> strings = new LinkedList<String>();
+//        strings.add("test1");
+//        strings.add("test2");
+//        strings.add("test3");
+//
+//        String result = (String) TestHelpers.invokePrivateMethod("createLineEntry", writer, strings);
+//
+//        Assert.assertEquals("test1,test2,test3", result);
+    }
+
+}
