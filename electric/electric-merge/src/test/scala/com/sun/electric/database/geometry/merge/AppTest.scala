7314486e2e8ba94df2a5eca3c547e64c48c315ea
nadezhin <nadezhin@b64ed6c1-75a8-e629-b1da-d0dae74a743a>
2010-12-02 08:14:03 +0000

    IMP: (12/1/10, DN) Rename package com.sun.electric.scala to com.sun.electric.database.geometry.merge .
    git-svn-id: https://svn.java.net/svn/electric~svn/trunk@12576 b64ed6c1-75a8-e629-b1da-d0dae74a743a

---
+++ electric/electric-merge/src/test/scala/com/sun/electric/database/geometry/merge/AppTest.scala
@@ -0,0 +1,17 @@
+package com.sun.electric.database.geometry.merge
+
+import org.junit._
+import Assert._
+
+@Test
+class AppTest {
+
+    @Test
+    def testOK() = assertTrue(true)
+
+//    @Test
+//    def testKO() = assertTrue(false)
+
+}
+
+
